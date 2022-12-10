22f737ceb2d085f79e8d0b76ce6ad8dbe5edd684
ravjanskie <jan2skie@gmail.com>
2022-12-10 18:51:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,9 +12,6 @@ import reportWebVitals from "./reportWebVitals";
 
 import axios from 'axios'
 
-
-
-
 //Routes import
 import LoginView from "./routes/LoginView";
 import MenuView from "./routes/MenuView";
@@ -23,7 +20,7 @@ import CodeView from "./routes/CodeView";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
 // axios.defaults.baseURL = 'http://localhost:4000/api';
-axios.defaults.baseURL = 'http://192.168.18.151:4000/api';
+axios.defaults.baseURL = 'http://192.168.100.7:4000/api';
 
 const router = createBrowserRouter([
   {
